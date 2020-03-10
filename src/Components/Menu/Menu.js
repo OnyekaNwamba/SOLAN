7ab5c1fbd9d2c06b9e199d1bb4fc74f52a353250
Lenard Pratt <striderman34@gmail.com>
2020-03-10 12:16:21 +0000

    Refractor

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,17 +1,50 @@
-import React from "react";
-import './menu.css';
-import current from './current.png';
-class Menu extends React.Component {
-    render() {
-        return(
-            <div className={"menu-bar"}>
-                <p className={'today'}>Today</p>
-                <img className={"current"} src={current} />
-                <p className={'schedule'}>Schedule</p>
-                <p className={'next-5-days'}> Next 5 days </p>
-            </div>
-        );
-    }
-}
+import "./menu.css";
+
+import React, { useState } from "react";
+
+import { Link } from "react-router-dom";
+
+const items = [
+  {
+    text: "Today",
+    path: "/"
+  },
+  {
+    text: "Schedule",
+    path: "/schedule"
+  },
+  {
+    text: "Next 5 days",
+    path: "/next"
+  }
+];
+
+const Menu = () => {
+  const [selected, setSelected] = useState(0);
+
+  const handleClick = index => {
+    setSelected(index);
+  };
+
+  const listItems = items.map((item, index) => (
+    <Link to={item.path}>
+      <li
+        key={item}
+        onClick={() => {
+          handleClick(index);
+        }}
+        className={`${selected === index ? "selected pa-1" : "pa-1"}`}
+      >
+        {item.text}
+      </li>
+    </Link>
+  ));
+
+  return (
+    <div className={"menu-bar"}>
+      <ul>{listItems}</ul>
+    </div>
+  );
+};
 
 export default Menu;
--- src/Components/WindRain/WindRain.js
+++ src/Components/WindRain/WindRain.js
@@ -1,21 +1,20 @@
-import React from "react";
-import './wind-rain.css';
-import rainImage from './Vector.png';
-import divisorImage from './dividor.png'
-import windImage from './wind-image.svg';
+import "./wind-rain.css";
 
-class WindRain extends React.Component {
-    render() {
-        return(
-            <div className={'bar'}>
-                <p  className={'rain'}>69%</p>
-                <img className={'rain-image'} src={rainImage} />
-                <img  className={'dividor'} src={divisorImage}/>
-                <p className={'wind'}>69 mph</p>
-                <img className={'wind-image'} src={windImage}/>
-            </div>
-        );
-    }
-}
+import React from "react";
+import divisorImage from "./dividor.png";
+import rainImage from "./Vector.png";
+import windImage from "./wind-image.svg";
+
+const WindRain = ({ speed }) => {
+  return (
+    <div className={"bar"}>
+      <p className={"rain"}>69%</p>
+      <img className={"rain-image"} src={rainImage} />
+      <img className={"dividor"} src={divisorImage} />
+      <p className={"wind"}>{speed}</p>
+      <img className={"wind-image"} src={windImage} />
+    </div>
+  );
+};
 
 export default WindRain;
